a4eb6df34d9c05ebd4cadd421bd6893021a5ab36
jtnardin <john.nardini@colorado.edu>
2016-03-24 11:13:12 -0600

    Now accounts for positive and negative v

--- test_problem_1.m
+++ test_problem_1.m
@@ -3,7 +3,7 @@
 
 %only valid for V >= 0!
 
-n = 100;
+n = 150;
 dt = 1e-3;
 
 x = linspace(0,1,n);
@@ -13,13 +13,13 @@ t = 0:dt:1;
 xn = length(x);
 tn = length(t);
 
-x_int = 3:xn-2;
+x_int = 2:xn-1;
 
 
 D = 6e-3;
-V = 2;
-x0 = 0.2;
-theta = 0.5;
+V = -2;
+x0 = 0.8;
+theta = 1;
 
 Dc = D*dt/dx^2;
 Vc = V*dt/dx;
@@ -31,6 +31,8 @@ LB = @(t) 1/sqrt(1+4*t).*exp(-(x0+V*t).^2./(D*(1+4*t)));
 %right boundary
 RB = @(t) 1/sqrt(1+4*t).*exp(-(1-x0-V*t).^2./(D*(1+4*t)));
 
+exact_soln = @(t) 1/sqrt(1+4*t)*exp(-(x-x0-V*t).^2/(D*(1+4*t)));
+
 %sigma for flux limiters
 sigma = @(r) (r+abs(r))./(1+abs(r));
 
@@ -38,12 +40,21 @@ sigma = @(r) (r+abs(r))./(1+abs(r));
 % A = @(s_l,s_r) sparse([x_int x_int x_int],[x_int-1 x_int x_int+1],[(Dc + Vc*(-1 + s_r)) ...
 %     (1-2*Dc+Vc*(1-s_l/2-s_r/2)) (Dc-Vc*s_l/2)],xn,xn);
 
-A_n = @(se,sw) sparse([x_int x_int x_int  1 2 xn-1 xn],[x_int-1 x_int x_int+1  1 2 xn-1 xn],[(-Vc*theta+Vc*theta*sw/2); ...
-    (1+Vc*theta-Vc*theta*se/2-Vc*theta*sw/2); (Vc*theta*se/2); ones(4,1)],xn,xn);
+A_np = @(se,sw) sparse([x_int x_int x_int  1 xn],[x_int-1 x_int x_int+1  1 xn],[(-Vc*theta+Vc*theta*sw/2); ...
+    (1+Vc*theta-Vc*theta*se/2-Vc*theta*sw/2); (Vc*theta*se/2); ones(2,1)],xn,xn);
 
-A_nm1 = @(se,sw) sparse([x_int x_int x_int 1 2 xn-1 xn],[x_int-1 x_int x_int+1  1 2 xn-1 xn],...
+A_nm1p = @(se,sw) sparse([x_int x_int x_int 1 xn],[x_int-1 x_int x_int+1  1 xn],...
     [(Dc+(1-theta)*Vc-(1-theta)*Vc*sw/2); (1-2*Dc-(1-theta)*Vc+(1-theta)*Vc*se/2+(1-theta)*Vc*sw/2); ...
-    (Dc-(1-theta)*Vc*se/2); ones(4,1)],xn,xn);
+    (Dc-(1-theta)*Vc*se/2); ones(2,1)],xn,xn);
+
+A_nn = @(se,sw) sparse([x_int x_int x_int  1 xn],[x_int-1 x_int x_int+1  1 xn],[(-theta*Vc*sw/2); ...
+    (1+theta*Vc*se/2+theta*Vc*sw/2-theta*Vc); (theta*Vc-Vc*theta*se/2); ones(2,1)],xn,xn);
+
+A_nm1n = @(se,sw) sparse([x_int x_int x_int 1 xn],[x_int-1 x_int x_int+1  1 xn],...
+    [(Dc+(1-theta)*Vc*sw/2); (1-2*Dc+(1-theta)*Vc-(1-theta)*Vc*se/2-(1-theta)*Vc*sw/2); ...
+    (Dc-(1-theta)*Vc+(1-theta)*Vc*se/2); ones(2,1)],xn,xn);
+
+
 
 %initialize
 u = zeros(xn,tn);
@@ -51,16 +62,39 @@ u(:,1) = IC(x);
 
 for i = 2:tn
     
-    r_e = (u(x_int,i-1) - u(x_int-1,i-1))./(u(x_int+1,i-1) - u(x_int,i-1));    
-    r_w = (u(x_int-1,i-1) - u(x_int-2,i-1))./(u(x_int,i-1) - u(x_int-1,i-1));
+    if V>=0
     
-    u(:,i) = A_n(sigma(r_e),sigma(r_w))\A_nm1(sigma(r_e),sigma(r_w))*u(:,i-1);
+        r_e = (u(x_int,i-1) - u(x_int-1,i-1))./(u(x_int+1,i-1) - u(x_int,i-1));    
+        r_w = (u(x_int(2:end)-1,i-1) - u(x_int(2:end)-2,i-1))./(u(x_int(2:end),i-1) - u(x_int(2:end)-1,i-1));
+        r_w = [-1;r_w];
+
+        %compute interior points
+        u(:,i) = A_np(sigma(r_e),sigma(r_w))\A_nm1p(sigma(r_e),sigma(r_w))*u(:,i-1);
+        
+    elseif V<0
+        
+        r_e = (u(x_int(1:end-1)+1,i-1) - u(x_int(1:end-1)+2,i-1))./(u(x_int(1:end-1),i-1) - u(x_int(1:end-1)+1,i-1));
+        r_e = [r_e;-1];
+        r_w = (u(x_int,i-1) - u(x_int+1,i-1))./(u(x_int-1,i-1) - u(x_int,i-1));
+        
+        %compute interior points
+        u(:,i) = A_nn(sigma(r_e),sigma(r_w))\A_nm1n(sigma(r_e),sigma(r_w))*u(:,i-1);
+        
+        
+    end
+    
+    %boundary conditions
+    u(1,i) = LB(t(i));
+    u(end,i) = RB(t(i));
 end
 
 for i = 1:10:tn
     plot(x,u(:,i))
+    hold on
+    plot(x,exact_soln(t(i)),'r')
     title(num2str(i));
     axis([0 1 0 1])
     pause(.125)
+    hold off
 end
 
